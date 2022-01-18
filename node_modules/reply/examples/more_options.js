f76ea28912e387f4c6960c1c525782a9e3188f2b
João Augusto <joao.augusto@foodzilla.com.br>
2022-01-18 15:20:16 -0300

    feat: remove node_modules

--- node_modules/reply/examples/more_options.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var reply = require('./..');
-var options = JSON.parse(fs.readFileSync(__dirname + '/options.json').toString());
-
-reply.get(options, function(err, answers){
-  console.log('\n ==== Replies:\n');
-  if (err) console.log(err);
-  console.log(answers);
-});
